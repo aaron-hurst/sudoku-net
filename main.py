32981820591429483e303f6d73cdd1fbaf7ae8e5
aaron-hurst <a.hurst.eng@gmail.com>
2019-04-04 21:12:17 +0800

    Add mapping of sudokus to numpy arrays
    Sudokus are read from the data source file as strings of integers with each integer representing one cell. This commit adds code that maps this to a numpy array with each element representing a different cell.

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 from SolverNN import SolverNN
 
 # Parameters
-n_samples = 1000
+n_samples = 1
 test_set_size = 0.3
 sudoku_dim = 9*9
 n_nodes = [100]
 
-# Load sudoku puzzles
+# Load sudoku puzzles and convert them into numpy arrays within dataframe
 data = pd.read_csv("sudoku.csv", skiprows=0, nrows=n_samples, dtype=str)
+data = data.applymap(lambda x: np.array(list(map(int, x))))
 
 # Select train and test data
 data_train, data_test = train_test_split(data, test_size=test_set_size)
 
 # Initialise solver
 solver = SolverNN(sudoku_dim, sudoku_dim, n_nodes)
-
-# Initialise Sudoku objects for each puzzle
 
 # Train
 
